bfd159956488b66eb51d0d4f8cc1ac1c636f3095
peterneubauer <peter@neubauer.se>
2013-01-24 14:47:30 +0100

    fix build error

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ git pull
 mvn clean license:format package
 
 #make preview PROJECT=files
-make preview PROJECT=rest
-make preview PROJECT=cypher
+#make preview PROJECT=rest
+#make preview PROJECT=cypher
 make preview PROJECT=java-course
 #make preview PROJECT=ops-course
 #make preview PROJECT=python-course
