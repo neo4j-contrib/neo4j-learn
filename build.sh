bd85d32acfd9612bcd8f8a4c6bd5385776b0c6cf
Michael Hunger <github@jexp.de>
2013-01-24 12:38:55 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 cd $( dirname $0 )
 git reset --hard --
 git clean -d -x -f 
@@ -11,3 +12,5 @@ make preview PROJECT=cypher
 #make preview PROJECT=ops-course
 #make preview PROJECT=python-course
 #make preview PROJECT=cypher-course
+
+cp -r target/html/* /var/www/learn.neo4j.org
