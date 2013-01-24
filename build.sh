5cb6770de3ea18cc2a0a9d93b49aa72af188ba33
Michael Hunger <github@jexp.de>
2013-01-24 14:31:03 +0100

    removed rest and cypher builds

--- build.sh
+++ build.sh
@@ -7,11 +7,6 @@ git pull
 mvn clean license:format package
 
 #make preview PROJECT=files
-make preview PROJECT=rest
-make preview PROJECT=cypher
 make preview PROJECT=java-course
-#make preview PROJECT=ops-course
-#make preview PROJECT=python-course
-#make preview PROJECT=cypher-course
 
 cp -rv target/html/* /var/www/learn.neo4j.org
