a78b8b9be00d8ce224632573cc603559f512d6ed
peterneubauer <peter@neubauer.se>
2013-02-12 14:37:00 +0100

    initialize

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ git pull
 mvn clean license:format package
 
 make initialize preview project_name=java-hello-world
-make preview project_name=java-cypher
+make initialize preview project_name=java-cypher
 
 cp -rv target/html5/*/ /var/www/learn.neo4j.org
