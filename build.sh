654d4df90176bb150ad3ce41faf98015f2ee00f0
peterneubauer <peter@neubauer.se>
2013-02-12 14:00:25 +0100

    restrucutring for new doctools

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git clean -d -x -f
 git pull
 mvn clean license:format package
 
-make preview PROJECT=java-hello-world
-make preview PROJECT=java-cypher
+make preview project_name=java-hello-world
+make preview project_name=java-cypher
 
 cp -rv target/html/* /var/www/learn.neo4j.org
